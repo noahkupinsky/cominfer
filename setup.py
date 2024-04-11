d3073164ed2a2ddf70eb25ca0d0276e005dd9b71
Noah Kupinsky <noah@kupinsky.com>
2024-04-11 12:42:37 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cominfer',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'argparse',
+        'importlib-metadata; python_version<"3.8"',
+    ],
+    author='Noah Kupinsky',
+    author_email='noah@kupinsky.com',
+    description='A tool for synchronizing note files to GitHub repositories.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
