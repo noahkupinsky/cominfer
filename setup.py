65f56eb7dcd2c38ca6d243a3813b4c8ece2509b0
Noah Kupinsky <noah@kupinsky.com>
2024-04-12 09:42:43 -0400

    undo accidental changes to setup

--- setup.py
+++ setup.py
@@ -2,18 +2,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dickandballs',
+    name='cominfer',
     version='0.1',
     packages=find_packages(),
-    install_requires=['cominfer'],
+    install_requires=[
+        'argparse',
+        'importlib-metadata; python_version<"3.8"',
+    ],
     entry_points={
         'console_scripts': [
-            'dickandballs=dickandballs.command:main',
+            'cominfer=cominfer.package_creator:main',
         ],
     },
-    author='Your name here',
-    author_email='Your email here',
-    description='Your description here.',
+    author='Noah Kupinsky',
+    author_email='noah@kupinsky.com',
+    description='A tool for synchronizing note files to GitHub repositories.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 )
